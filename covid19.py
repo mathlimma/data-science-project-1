903343608cdba0fd8ba981d6f7a3bccdf5b05e34
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-08 10:38:02 -0300

    fix: covid19.py

--- covid19.py
+++ covid19.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from pylab import *
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 covid = pd.read_csv(r'caso.csv', encoding = "utf-8")
 
-plt.plot(covid['date'])
+plt.plot(((covid['city'] == 'Recife'))/len(covid['city']))
